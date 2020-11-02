22870da88dd9e645a73c1b390ec4d776bb1a9846
Tristan Seligmann <mithrandi@mithrandi.net>
2020-11-02 14:02:22 +0200

    Bump Hypothesis

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     extras_require={
         'test': [
             'testtools>=2.2.0',
-            'hypothesis>=3.6.0,<4.0.0',
+            'hypothesis>=3.6.0,<5.0.0',
             ],
         },
     )
